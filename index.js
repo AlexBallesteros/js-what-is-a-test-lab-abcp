f5d0049edbf1da781c883476f3c73fc65be1dfdd
Alejandro Ballesteros <ayb.021@gmail.com>
2018-01-15 00:47:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Joe"
-var height = 74 < 40
+var name = "Susan"
+var height = expect(74).toBeLessThan(40)
 var message = ` ${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
